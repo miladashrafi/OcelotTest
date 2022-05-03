79205278f460103850d6a2904cc3c93d2f89054d
mi.ashrafi <mi.ashrafi@alibaba.ir>
2022-05-03 18:34:39 +0430

    initial sample

---
+++ Test.ApiGateway/Program.cs
@@ -0,0 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("Ocelot.json");
+// Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseOcelot().Wait();
+
+app.Run();
